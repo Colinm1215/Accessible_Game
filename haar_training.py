8dbea5a28b5aa72192747cf31dba62ee278b0e0a
sashwat22 <Basketball2002>
2019-11-16 11:20:20 -0500

    a

--- haar_training.py
+++ haar_training.py
@@ -4,7 +4,7 @@ import time
 
 def get_gesture(gest1, gest2):
     if gest1 == "fist" and gest2 == "palm":
-        # osascript.osascript(""" bruh
+        # osascript.osascript(""" fkdlfjadsf
         # tell application "Tunify"
 	    #     pause
         # end tell
